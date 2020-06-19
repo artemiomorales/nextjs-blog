41851f726132fc42a5b8f424cc5cf4d02ebe4fae
Ricardo <ric.morales22@gmail.com>
2020-06-19 12:04:53 -0400

    Fix compile errors

--- pages/api/hello.js
+++
@@ -1,7 +0,0 @@
-// req - request data, res = response data
-import { NextApiRequst, NextApiResponse } from 'next'
-
-export default (_: NextApiRequst, res: NextApiResponse) => {
-  // ...
-  res.status(200).json({ text: 'Hello' })
-}
---
+++ pages/api/hello.ts
@@ -0,0 +1,7 @@
+// req - request data, res = response data
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default (_: NextApiRequest, res: NextApiResponse) => {
+  // ...
+  res.status(200).json({ text: 'Hello' })
+}
